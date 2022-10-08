fe03e6fd8713b783c7c8925781c5605967346f2d
claudvanbarret <claudivanbarretodasilva@gmail.com>
2022-10-08 17:54:20 -0300

    feat: add working hours columns

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,57 @@
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { CellProps, Column } from 'react-table';
+
+import { NumberCell } from '../../components/atoms/NumberCell';
+import { Table } from '../../components/molecules/Table';
+import useSpreads from '../../hooks/useSpreads';
+import { SpreadConfiguration } from '../../models/SpreadConfiguration';
+import { WorkingHoursTableAccessor } from './Home.types';
+
 const Home = () => {
+  const { workingHours, updateData } = useSpreads();
+  const { t } = useTranslation();
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: t('account_id'),
+        accessor: WorkingHoursTableAccessor.ACCOUNT_ID,
+      },
+      {
+        Header: t('symbol'),
+        accessor: WorkingHoursTableAccessor.SYMBOL,
+      },
+      {
+        Header: t('side'),
+        accessor: WorkingHoursTableAccessor.SIDE,
+      },
+      {
+        Header: t('notional_from'),
+        accessor: WorkingHoursTableAccessor.NOTIONAL_FROM,
+        width: 160,
+        Cell: (props: CellProps<SpreadConfiguration, number>) => <NumberCell {...props} onBlur={updateData} />,
+      },
+      {
+        Header: t('notional_to'),
+        accessor: WorkingHoursTableAccessor.NOTIONAL_TO,
+        width: 160,
+        Cell: (props: CellProps<SpreadConfiguration, number>) => <NumberCell {...props} onBlur={updateData} />,
+      },
+      {
+        Header: t('percent'),
+        accessor: WorkingHoursTableAccessor.SPREAD_PERCENTIL,
+        width: 160,
+        Cell: (props: CellProps<SpreadConfiguration, number>) => <NumberCell {...props} onBlur={updateData} />,
+      },
+    ],
+    []
+  );
+
   return (
     <>
-      <h1>Algotrading</h1>
+      <h1>{t('algotrading')}</h1>
+      <Table columns={columns as Column<SpreadConfiguration>[]} data={workingHours} height={300} />
     </>
   );
 };
---
+++ src/pages/Home/Home.types.ts
@@ -0,0 +1,8 @@
+export enum WorkingHoursTableAccessor {
+  ACCOUNT_ID = 'accountId',
+  SYMBOL = 'symbol',
+  SIDE = 'side',
+  NOTIONAL_FROM = 'notionalFrom',
+  NOTIONAL_TO = 'notionalTo',
+  SPREAD_PERCENTIL = 'spreadPercentil',
+}
